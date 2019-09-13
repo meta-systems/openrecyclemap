940cdf212faa23832be94cd308b6fb84389c5542
vitalcrazz <zojl@yandex.ru>
2019-09-13 16:49:14 +0300

    refresh form

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -62,17 +62,7 @@
                 adding: false,
                 marker: null,
                 sheet: false,
-                waste: {
-                    waste_disposal: false,
-                    plastic: false,
-                    paper: false,
-                    glass: false,
-                    metal: false,
-                    batteries: false,
-                    low_energy_bulbs: false,
-                    plastic_bags: false,
-                    plastic_bottles: false
-                }
+                waste: {}
             };
         },
         mixins: [overpassMixin, oauthMixin],
@@ -168,9 +158,23 @@
             },
             clearWaste: function () {
                 this.waste.waste_disposal = false;
+            },
+            initWaste: function () {
+                this.waste = {
+                    waste_disposal: false,
+                    plastic: false,
+                    paper: false,
+                    glass: false,
+                    metal: false,
+                    batteries: false,
+                    low_energy_bulbs: false,
+                    plastic_bags: false,
+                    plastic_bottles: false
+                };
             }
         },
         mounted() {
+            this.initWaste();
             this.authInit();
             this.setupMap();
             this.loadData();
@@ -185,6 +189,7 @@
                         component.map.removeLayer(component.marker);
                     }
                     component.marker = L.marker(e.latlng).addTo(component.map);
+                    component.initWaste();
                     component.sheet = true;
                     component.map.setView(e.latlng, 18, {animate: false});
                 }
