bc38953fcb02624374b54981530f4834b6ae0d08
vitalcrazz <zojl@yandex.ru>
2019-09-18 16:52:04 +0300

    install vue-analytics

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
 import colors from 'vuetify/lib/util/colors'
+import VueAnalytics from 'vue-analytics'
 
 Vue.use(Vuetify, {
     iconfont: 'md',
@@ -13,6 +14,11 @@ Vue.use(Vuetify, {
         accent: colors.shades.black,
         error: colors.red.accent3
     }
+});
+
+Vue.use(VueAnalytics, {
+    id: 'G-Q93XCW3E1M',
+    router
 });
 
 Vue.config.productionTip = false;
