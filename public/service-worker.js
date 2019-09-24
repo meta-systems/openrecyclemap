4f421f9a9cbfae2f08970fdaa916c0c9b3e1c6d4
vitalcrazz <zojl@yandex.ru>
2019-09-24 16:31:32 +0300

    sw caching policy

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,11 +12,15 @@ self.addEventListener('install', function(e) {
 self.addEventListener('fetch', function(event) {
 
     let url = new URL(event.request.url);
-    console.log(url.pathname);
 
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+            if(url.pathname === '/') {
+                return fetch(event.request) || response;
+            }
+            else {
+                return response || fetch(event.request);
+            }
         })
     );
 
