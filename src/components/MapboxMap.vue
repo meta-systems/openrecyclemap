9bbd9a8e936b24473a7f51058cc65b7229a9b5e8
vitalcrazz <zojl@yandex.ru>
2019-10-14 13:04:05 +0300

    mapbox: geolocation support

--- src/components/MapboxMap.vue
+++ src/components/MapboxMap.vue
@@ -50,7 +50,7 @@
             },
             showPosition: function () {
                 // request location update and set location
-                this.locateControl.start();
+                this.locateControl.trigger();
             },
             savePosition: function () {
                 let position = this.map.getCenter();
@@ -76,6 +76,14 @@
             },
             mapLoad: function () {
                 this.$emit('map-init', this.map);
+
+                this.locateControl = new mapboxgl.GeolocateControl({
+                    positionOptions: {
+                        enableHighAccuracy: true
+                    },
+                    trackUserLocation: true
+                });
+                this.map.addControl(this.locateControl);
 
                 this.map.addLayer(this.highlightedMarkerStyle(), 'recycling-russia');
 
@@ -141,6 +149,9 @@
         top:21px;
         right:20px;
         background-image: url("data:image/svg+xml;charset=utf8,%3Csvg width='50' height='50' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M22.47 21.38L9.23 27.52C8 28.09 8 29 9.23 29.58l13.24 6.13c1.23.57 3.22.57 4.45 0l13.24-6.13c1.23-.57 1.23-1.5 0-2.06l-13.24-6.14a6.02 6.02 0 0 0-4.45 0z' fill='%23B3B3B3'/%3E%3Cpath d='M22.47 13.03L9.23 19.16c-1.23.57-1.23 1.5 0 2.06l13.24 6.14c1.23.56 3.22.56 4.45 0l13.24-6.14c1.23-.57 1.23-1.49 0-2.06l-13.24-6.13a6.02 6.02 0 0 0-4.45 0z' fill='%239C9C9C' fill-opacity='.55'/%3E%3C/svg%3E");
+    }
+    .mapboxgl-ctrl-geolocate {
+        display: none !important;
     }
 
     .add_mode .orm_zoom {
